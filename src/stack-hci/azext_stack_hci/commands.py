3289bf47560b7969e7518bdd4fae554f5dccd197
kai ru <kairu@microsoft.com>
2024-12-03 11:54:34 +0800

    add space line

--- src/stack-hci/azext_stack_hci/commands.py
+++ src/stack-hci/azext_stack_hci/commands.py
@@ -14,6 +14,7 @@
 def load_command_table(self, _):  # pylint: disable=unused-argument
 
     with self.command_group('stack-hci cluster'):
+
         from azext_stack_hci.custom import ClusterList, ClusterCreate, IdentityAssign, IdentityRemove
         self.command_table['stack-hci cluster list'] = ClusterList(loader=self)
         self.command_table['stack-hci cluster create'] = ClusterCreate(loader=self)
